9a35e651069796cd3f4c27fdd3142a845942b4c3
RangelJhoan <jhoanrangels2@gmail.com>
2022-05-11 16:52:04 -0500

    Se finalizó el módulo

--- contactos.c
+++ contactos.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
 
 #define TAMANO_USU 10
 
 int posicion = 0;
 
-struct Contacto{
+struct Contacto
+{
     int codigo;
     char nombre[40];
     char apellido[40];
@@ -14,7 +15,8 @@ struct Contacto{
     char numeroTelefono[12];
 };
 
-struct Usuario{
+struct Usuario
+{
     int codigo;
     char nombre[40];
     char apellido[40];
@@ -22,7 +24,8 @@ struct Usuario{
     struct Contacto contacto[TAMANO_USU];
 };
 
-struct Contacto registrarContacto(){
+struct Contacto registrarContacto()
+{
 
     struct Contacto contacto;
 
@@ -49,7 +52,8 @@ struct Contacto registrarContacto(){
     return contacto;
 }
 
-struct Usuario registrarUsuario(){
+struct Usuario registrarUsuario()
+{
     struct Usuario usuario;
 
     printf("Ingrese el codigo del usuario: ");
@@ -71,23 +75,66 @@ struct Usuario registrarUsuario(){
     return usuario;
 }
 
-void imprimirUsuario(struct Usuario usuario){
+void editarContacto(struct Usuario *usuario, int codigoContacto)
+{
+    int existeContacto = 0;
+    for (int i = 0; i < posicion; i++)
+    {
+        if (codigoContacto == (*usuario).contacto[i].codigo)
+        {
+            printf("Ingrese los nuevos datos del contacto: \n");
+            (*usuario).contacto[i] = registrarContacto();
+            printf("\nContacto editado correctamente.\n");
+            existeContacto = 1;
+        }
+    }
+    if (existeContacto == 0)
+    {
+        printf("\nNo se encontro el usuario.\n");
+    }
+}
+
+void eliminarContacto(struct Usuario *usuario, int codigoContacto)
+{
+    int existeContacto = 0;
+    for (int i = 0; i < posicion; i++)
+    {
+        if (codigoContacto == (*usuario).contacto[i].codigo)
+        {
+            for (int j = i; j < TAMANO_USU; j++)
+            {
+                (*usuario).contacto[j] = (*usuario).contacto[j + 1];
+            }
+            printf("\nContacto eliminado correctamente.\n");
+            posicion--;
+            existeContacto = 1;
+        }
+    }
+    if (existeContacto == 0)
+    {
+        printf("\nNo se encontro el usuario.\n");
+    }
+}
+
+void imprimirUsuario(struct Usuario usuario)
+{
     printf("\n%s, tus contactos son:\n", usuario.nombre);
-    for(int i=0; i<posicion; i++){
+    for (int i = 0; i < posicion; i++)
+    {
         printf("Codigo: %i: Nombre: %s. Apellido: %s. Referencia: %s. Numero: %s\n", usuario.contacto[i].codigo, usuario.contacto[i].nombre, usuario.contacto[i].apellido, usuario.contacto[i].referencia, usuario.contacto[i].numeroTelefono);
     }
 }
 
-int main(){
-
+int main()
+{
     struct Usuario usuario;
-
-    usuario = registrarUsuario();
-
+    // usuario = registrarUsuario(); Obsoleto, falta agregar la lista de usuarios (tipo login)
     int decision;
+    int codigoUsuario;
 
-    do{
-        printf("\nQue accion desea realizar? \n1. Agregar contacto. \n2. Visualizar mis contactos. \n0. Salir.\n");
+    do
+    {
+        printf("\nQue accion desea realizar? \n1. Agregar contacto. \n2. Visualizar mis contactos. \n3. Editar contacto. \n4. Eliminar contacto. \n0. Salir.\n");
         scanf("%i", &decision);
         fflush(stdin);
 
@@ -101,6 +148,16 @@ int main(){
         case 2:
             imprimirUsuario(usuario);
             break;
+        case 3:
+            printf("\nIngrese el codigo del contacto a editar: ");
+            scanf("%i", &codigoUsuario);
+            editarContacto(&usuario, codigoUsuario);
+            break;
+        case 4:
+            printf("\nIngrese el codigo del contacto a eliminar: ");
+            scanf("%i", &codigoUsuario);
+            eliminarContacto(&usuario, codigoUsuario);
+            break;
         case 0:
             printf("Hasta luego");
             break;
@@ -109,7 +166,7 @@ int main(){
             break;
         }
 
-    }while(decision!=0);
+    } while (decision != 0);
 
     return 0;
 }
